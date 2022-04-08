4a840840ebe1731a44f1b07be84cdd4d9403c184
Andrey Zaderenko <andrey42work@gmail.com>
2022-04-08 22:35:47 +0300

    create project

---
+++ test1.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+let number = 5;
+const leftBorderWidth = 1; 
+
+number = 117;
+console.log(number, leftBorderWidth);
+
+const category = 'roys';
+
+console.log(`http://url.com/${category}/5/5`)
